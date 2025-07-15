adfb3a9d3545144239a1cf62bc2321816d129f3f
ItzKeeni <maratkanovartem89@mail.ru>
2025-07-15 14:03:35 +0300

    add 404 and favicon

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     <div className="bg-main-dark p-4 fixed w-full z-[99999] shadow-2xl">
         <a className="text-2xl sm:text-3xl" href="/">
             <span className="text-logo-green">B</span><span className="text-logo-blue">GDPS</span>
-            <span className="text-logo-red ml-2">demon List</span>
+            <span className="text-logo-red ml-2">DemonList</span>
         </a>
     </div>
   )
---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <main className="flex flex-1 flex-col items-center justify-center mt-20">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p>Not Found</p>
+    </main>
+  );
+};
+
+export default NotFound;
+
