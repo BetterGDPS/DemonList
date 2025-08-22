e5221b4df3772549e339b8ce5fda749bbbdadfc3
ItzKeeni <maratkanovartem89@mail.ru>
2025-08-22 14:07:44 +0300

    change favcion and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,10 @@ const kronaOne = Krona_One({
 });
 
 export const metadata: Metadata = {
-  title: "BGDPS DemonList",
+  title: "GlobalList",
   description: "Официальный топ уровней BGDPS",
   icons: {
-    icon: "/icon.png"
+    icon: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1nbG9iZS1pY29uIGx1Y2lkZS1nbG9iZSI+PGNpcmNsZSBjeD0iMTIiIGN5PSIxMiIgcj0iMTAiLz48cGF0aCBkPSJNMTIgMmExNC41IDE0LjUgMCAwIDAgMCAyMCAxNC41IDE0LjUgMCAwIDAgMC0yMCIvPjxwYXRoIGQ9Ik0yIDEyaDIwIi8+PC9zdmc+"
   },
 };
 
